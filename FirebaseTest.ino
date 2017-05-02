2135884919760ba06618e82f6ffa7f2a33b0e2fc
MK Hejazy <ultimate.zero1994@gmail.com>
2017-05-02 10:24:29 +0200

    Fully working

--- FirebaseTest.ino
+++ FirebaseTest.ino
@@ -1,162 +1,241 @@
-#include <SparkFunESP8266WiFi.h>
-#include <LiquidCrystal.h>
-LiquidCrystal lcd(8, 9, 10, 11, 12, 13);
-#define LEVER_SWITCH_PIN 6
-#define echoPin 3 // Echo Pin
-#define trigPin 2 // Trigger Pin
+
+#define echoPin 52 // Echo Pin for ultrasonic
+#define trigPin 53 // Trigger Pin for ultrasonic
+#define buttonPin 41
+#define CAN_FULL_CAPACITY  40 // in CM
 #define SSID "ssid"
-#define PASS "password"
-#define DST_IP "54.247.71.175" //heroku server
+#define PASS "pass"
+#define DST_IP "46.137.108.231" //aast.voidbits.co
 #define DST_PORT 80
-#define MiddleRate 25
-#define FullRate 5
-#define CANCapacity 100
+#define BAUD_RATE 115200
+#define esp Serial3
 
+String resp; //temp response buffer
 
+bool isConnected = false;
+int isBtnPressed = 0;
+int lastCapacity = 0;
 
-const int BinId = 1;
-int pressSwitch = 0;
-
-ESP8266Client client;
 void setup() {
-  //LCD and switch
-  lcd.begin(16, 2);
-  lcd.clear();
-  lcd.print("Setting up.. Id=1");
+  //Setup ultrasonic
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
-  pinMode(LEVER_SWITCH_PIN, INPUT);
+  //
+  //Button
+  pinMode(buttonPin, INPUT);
   //
 
-  Serial.begin(115200);
-  while (esp8266.begin() != true)
-  {
-    Serial.println("Error connecting to ESP8266.");
-    delay(1000);
-  }
-  Serial.println("Connected to ESP8266.");
 
-  if (esp8266.status() <= 0)
-  {
-    while (esp8266.connect(SSID, PASS) < 0)
-      delay(1000);
-    Serial.println("Connected to WiFi");
+  Serial.begin(BAUD_RATE);
+  esp.begin(BAUD_RATE);
+
+  //  while (!Serial);
+  while (!esp);
+
+  Serial.println("Recycle bin monitor");
+
+
+  disconnectIfConnected();
+  delay(100);
+  resp = send_cmd("AT");
+  //  Serial.println(resp);
+  if (resp.indexOf("OK") >= 0) {
+    Serial.println("ESP is ready");
+
+    resp = send_cmd("AT+CWJAP?");
+    //    Serial.println(resp);
+    if (resp.indexOf("No AP") >= 0) {
+      Serial.println("Connecting to WiFi");
+      connectToWifi();
+    }
+    else {
+      Serial.println("Already connected to WiFi");
+    }
+
+    connectToServer();
   }
   else {
-    Serial.println("Already Connected to WiFi");
+    Serial.println("Error: ESP8266 is not OK. Restart Arduino.");
+    while (1);
   }
-  delay(1000);
 
-//  clientSetup();
+
 }
 
 
-void sendCapacity() {
-  // ESP8266Client connect([server], [port]) is used to 
-  // connect to a server (const char * or IPAddress) on
-  // a specified port.
-  // Returns: 1 on success, 2 on already connected,
-  // negative on fail (-1=TIMEOUT, -3=FAIL).
-  int retVal = client.connect(DST_IP, DST_PORT);
-  if (retVal <= 0)
-  {
-    Serial.print(F("Failed to connect to server. Code: "));
-    Serial.println(retVal);
+
+int ticks = 0;
+int currentCapacity;
+int btnState = 0;
+void loop() {
+  //  while (Serial.available()) {
+  //    esp.write(Serial.read());
+  //  }
+  //
+  //  while (esp.available())
+  //    Serial.write(esp.read());
+
+  currentCapacity = calculatePercentage();
+  isBtnPressed = digitalRead(buttonPin);
+  Serial.println(ticks);
+  Serial.print("Button: ");
+  Serial.println(isBtnPressed == 0 ? "pressed" : "not pressed");
+  Serial.print("Capacity: ");
+  Serial.print(currentCapacity);
+  Serial.println("%");
+  Serial.println();
+  if (needUpdate()) {
+    trySend();
+    ticks = 0;
+    lastCapacity = currentCapacity;
+    btnState = isBtnPressed;
     return;
   }
-  Serial.println("Connect to server successfully");
-  // print and write can be used to send data to a connected
-  // client connection.
-  client.print(getCapacityHttp());
-  delay(3000);
-    // available() will return the number of characters
-  // currently in the receive buffer.
-  receivePackets();
-  
-  // connected() is a boolean return value - 1 if the 
-  // connection is active, 0 if it's closed.
-//  if (client.connected())
-//    client.stop(); // stop() closes a TCP connection.
-}
-
-void receivePackets() {
-    while (client.available())
-      Serial.write(client.read()); // read() gets the FIFO char
-  
-}
-
-
-String getCapacityHttp() {
-   String httpRequest = "GET /api/bins/1?&open=false&capacity=";
-   httpRequest += calculatePercentage();
-   httpRequest += " HTTP/1.1\r\n";
-   httpRequest += "Host: aast.voidbits.co\r\n";
-   httpRequest += "Connection: keep-alive\r\n\r\n";
-   return httpRequest;
-}
-
-
-void loop() {
-  // Wait for client connections
-//  serverReceive();
-//  // Print on LCD
-//  pressSwitch = digitalRead(LEVER_SWITCH_PIN);
-//  if (pressSwitch)
-//  {
-//    Serial.println("pressed");
-//    int distance = calculateDInCM();
-//    if (distance <= FullRate)
-//    {
-//      render("Full", "Closed", distance);
-//    }
-//    else if (distance <= MiddleRate && distance > FullRate)
-//    {
-//      render("Available", "Closed", distance);
-//    }
-//    else if (distance > MiddleRate)
-//    {
-//      render("Available", "Closed", distance);
-//    }
-//  }
-//  else
-//  {
-//    render("Please close", "Opened", 0);
-//  }
-  if(client.connected()) {
-    Serial.println("Is connected");
-    receivePackets();
+  lastCapacity = currentCapacity;
+  btnState = isBtnPressed;
+  delay(1000);
+  if (ticks >= 8) {
+    ticks = 0;
+    trySend();
   }
   else {
-    Serial.println("Not connected. Sending..");
-    sendCapacity(); //start
+    ticks += 1;
   }
+
+}
+
+boolean needUpdate() {
+  int diff = lastCapacity - currentCapacity;
+  if(diff < 0) diff = diff * -1;
   
+  return (diff > 5) || (btnState != isBtnPressed);
+}
+
+void trySend() {
+  if (!isConnected) {
+    disconnectIfConnected();
+    connectToServer();
+  }
+  else
+    sendHttp();
+}
+
+void connectToWifi() {
+  String s = "AT+CWJAP=\"";
+  s += SSID;
+  s += "\",\"";
+  s += PASS;
+  s += "\"";
+  resp = send_cmd_delay(s, 4000); // give it 4 sec
+  if (resp.indexOf("GOT IP") >= 0) {
+    Serial.print("Connected to WiFi successfully");
+  }
+  else {
+    Serial.println("Error: Couldn't connect to WiFi. Restart Arduino.");
+    while (1);
+  }
+
+}
+
+void disconnectIfConnected() {
+  resp = send_cmd("AT+CIPCLOSE");
+  //  Serial.println(resp);
+
+}
+
+int retries = 0;
+void connectToServer() {
+  String s = "AT+CIPSTART=\"TCP\",\"";
+  s += DST_IP;
+  s += "\",";
+  s += DST_PORT;
+  retries += 1;
+  resp = send_cmd_delay(s, 2000);
+  //  Serial.println(resp);
+  if (resp.indexOf("OK") >= 0) {
+    retries = 0;
+    Serial.println("Connected successfully");
+    isConnected = true;
+    //    sendHttp();
+  }
+  else {
+    Serial.println(resp);
+    Serial.println("Error: Couldn't connect to server");
+    if (retries >= 5) {
+      Serial.println("Error: Couldn't connect after 5 retries. Check the address/port and restart Arduino");
+      while (1);
+    }
+    else {
+      connectToServer();
+    }
+  }
+}
+
+void sendHttp() {
+  if (!isConnected) return;
+  isBtnPressed = digitalRead(buttonPin);
+  String r = "GET /api/bins/1?capacity=";
+  r += currentCapacity;
+  r += "&open=";
+  r += (isBtnPressed == 0 ? "false" : "true");
+  r += " HTTP/1.1\r\nHost: aast.voidbits.co\r\nConnection: keep-alive\r\n\r\n";
+  String s = "AT+CIPSEND=";
+  s += r.length();
+
+  resp = send_cmd(s);
+  //  Serial.println(resp);
+  Serial.println("Sending update..");
+  esp.print(r);
+
+  Serial.println("Done sending");
   delay(1000);
+  boolean ok = false;
+  while (esp.available()) {
+    if (esp.find("OK")) {
+      ok = true;
+      Serial.println("Got OK response");
+      return;
+    }
+  }
+  if (!ok) {
+    Serial.println("No OK response...");
+    isConnected = false;
+  }
+}
+
+
+
+char c;
+String send_cmd(String cmd) {
+  esp.print(cmd);
+  esp.print("\r\n");
+  delay(500);
+  String output = "";
+  while (esp.available()) {
+    c = esp.read();
+    output += c;
+  }
+  return output;
+}
+
+String send_cmd_delay(String cmd, int d) {
+  esp.print(cmd);
+  esp.print("\r\n");
+  delay(d);
+  String output = "";
+  while (esp.available()) {
+    c = esp.read();
+    output += c;
+  }
+  return output;
 }
 
 
 int calculatePercentage() {
-  int distance = calculateDInCM();
-  return ((distance * CANCapacity) / 100);
-}
-
-void render(String state, String pos, int distance)
-{
-  Serial.println("render called");
-  lcd.clear();
-  lcd.setCursor(0, 0);
-  if (state.equals("Available"))
-  {
-    lcd.print(state + ((distance * CANCapacity) / 100) + "%");
-    lcd.setCursor(0, 1);
-    lcd.print(pos);
-  }
-  else
-  {
-    lcd.print(state);
-    lcd.setCursor(0, 1);
-    lcd.print(pos);
-  }
+  int cm = calculateDInCM();
+  if (cm > CAN_FULL_CAPACITY)
+    cm = CAN_FULL_CAPACITY;
+  return (cm * 100.0 / CAN_FULL_CAPACITY);
 }
 
 int calculateDInCM()
